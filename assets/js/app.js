eb572f1e074137b0340fb3a832e2229040b6a6e2
Aneleaigil <ligiaelena11@gmail.com>
2018-07-12 10:00:50 -0400

    subiendo archivos

---
+++ assets/js/app.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    $("#hide").click(function(){
+        $("worker-2").hide();
+    });
+    $("#show").click(function(){
+        $("worker-2").show();
+    });
+});
